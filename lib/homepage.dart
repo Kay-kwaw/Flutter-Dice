1b02a00e7d921c4e5b295a1d54b26470c2975d5e
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-04-05 12:00:03 +0000

    functions

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,6 +12,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -20,11 +27,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                //Setstate is a function that will rebuild the widget tree once the state is changed.
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  print('Left button got pressed.');
-                });
+               changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -32,10 +35,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
-                print('Right button got pressed.');
+                changeDiceFace();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
